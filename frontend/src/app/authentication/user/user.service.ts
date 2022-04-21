350af0f18d3404ecf92adee86b711bddfcfad849
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-04-21 19:49:52 -0300

    implementing token

--- frontend/src/app/authentication/user/user.service.ts
+++ frontend/src/app/authentication/user/user.service.ts
@@ -5,32 +5,32 @@ import { TokenService } from '../token/token.service';
 import { User } from './user';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class UserService {
-    private userSubject = new BehaviorSubject<User>({});
+  private userSubject = new BehaviorSubject<User>({});
 
-    constructor(private tokenService: TokenService) {
-        if (this.tokenService.hasToken()) {
-            this.decodeJWT();
-        }
+  constructor(private tokenService: TokenService) {
+    if (this.tokenService.hasToken()) {
+      this.decodeJWT();
     }
+  }
 
-    private decodeJWT() {
-        const token = this.tokenService.getToken();
-        const user = jwt_decode(token) as User;
-    }
+  private decodeJWT() {
+    const token = this.tokenService.getToken();
+    const user = jwt_decode(token) as User;
+  }
 
-    getUser() {
-        return this.userSubject.asObservable();
-    }
+  getUser() {
+    return this.userSubject.asObservable();
+  }
 
-    saveToken(token: string) {
-        this.tokenService.saveToken(token);
-        this.decodeJWT();
-    }
+  saveToken(token: string) {
+    this.tokenService.saveToken(token);
+    this.decodeJWT();
+  }
 
-    isLoggedIn() {
-        return this.tokenService.hasToken();
-    }
+  isLoggedIn() {
+    return this.tokenService.hasToken();
+  }
 }
