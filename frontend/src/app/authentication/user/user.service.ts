26a6210adc739ff20703a8368a0c3164a7433370
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-04-21 18:36:59 -0300

    implementing token

---
+++ frontend/src/app/authentication/user/user.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import jwt_decode from 'jwt-decode';
+import { BehaviorSubject } from 'rxjs';
+import { TokenService } from '../token/token.service';
+import { User } from './user';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class UserService {
+    private userSubject = new BehaviorSubject<User>({});
+
+    constructor(private tokenService: TokenService) {
+        if (this.tokenService.hasToken()) {
+            this.decodeJWT();
+        }
+    }
+
+    private decodeJWT() {
+        const token = this.tokenService.getToken();
+        const user = jwt_decode(token) as User;
+    }
+
+    getUser() {
+        return this.userSubject.asObservable();
+    }
+
+    saveToken(token: string) {
+        this.tokenService.saveToken(token);
+        this.decodeJWT();
+    }
+
+    isLoggedIn() {
+        return this.tokenService.hasToken();
+    }
+}
--- frontend/src/app/home/login/login.component.ts
+++ frontend/src/app/home/login/login.component.ts
@@ -1,34 +1,34 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { AuthenticationService } from "./../../authentication/authentication.service";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from './../../authentication/authentication.service';
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.css"],
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  username = "";
-  password = "";
+    username = '';
+    password = '';
 
-  constructor(
-    private authenticationService: AuthenticationService,
-    private router: Router
-  ) {}
+    constructor(
+        private authenticationService: AuthenticationService,
+        private router: Router,
+    ) {}
 
-  ngOnInit(): void {}
+    ngOnInit(): void {}
 
-  login() {
-    this.authenticationService
-      .authenticate(this.username, this.password)
-      .subscribe(
-        () => {
-          this.router.navigate(["products"]);
-        },
-        (error) => {
-          alert("User or password invalid");
-          console.error(error);
-        }
-      );
-  }
+    login() {
+        this.authenticationService
+            .authenticate(this.username, this.password)
+            .subscribe(
+                () => {
+                    this.router.navigate(['products']);
+                },
+                (error) => {
+                    alert('User or password invalid');
+                    console.error(error);
+                },
+            );
+    }
 }
