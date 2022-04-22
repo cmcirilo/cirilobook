ce4381ce4b97179d7f20dc3cfd839cbc50f10141
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-04-22 16:59:46 -0300

    using pipe and operator switchMap

--- frontend/src/app/products/list-products/list-products.component.ts
+++ frontend/src/app/products/list-products/list-products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { switchMap } from 'rxjs/operators';
 import { Products } from '../product';
 import { ProductsService } from '../products.service';
 import { UserService } from './../../authentication/user/user.service';
@@ -9,7 +11,7 @@ import { UserService } from './../../authentication/user/user.service';
   styleUrls: ['./list-products.component.css'],
 })
 export class ListProductsComponent implements OnInit {
-  products!: Products;
+  products$!: Observable<Products>;
 
   constructor(
     private userService: UserService,
@@ -17,11 +19,11 @@ export class ListProductsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.userService.getUser().subscribe((user) => {
-      const userName = user.name ?? '';
-      this.productsService.userList(userName).subscribe((products) => {
-        this.products = products;
-      });
-    });
+    this.products$ = this.userService.getUser().pipe(
+      switchMap((user) => {
+        const userName = user.name ?? '';
+        return this.productsService.userList(userName);
+      })
+    );
   }
 }
