f18d91922ca7161457773aca6e91d4ce98274320
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-06-08 11:47:51 -0300

    creating detail product component

--- frontend/src/app/products/products-routing.module.ts
+++ frontend/src/app/products/products-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailProductComponent } from './detail-product/detail-product.component';
 import { ListProductsComponent } from './list-products/list-products.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ListProductsComponent,
+  },
+  {
+    path: ':productId',
+    component: DetailProductComponent,
   },
 ];
 
--- frontend/src/app/products/products.module.ts
+++ frontend/src/app/products/products.module.ts
@@ -5,12 +5,14 @@ import { GridPhotoProductsComponent } from './grid-photo-products/grid-photo-pro
 import { ListProductsComponent } from './list-products/list-products.component';
 import { ProductComponent } from './product/product.component';
 import { ProductsRoutingModule } from './products-routing.module';
+import { DetailProductComponent } from './detail-product/detail-product.component';
 
 @NgModule({
   declarations: [
     ListProductsComponent,
     ProductComponent,
     GridPhotoProductsComponent,
+    DetailProductComponent,
   ],
   imports: [CommonModule, ProductsRoutingModule, CardModule],
 })
