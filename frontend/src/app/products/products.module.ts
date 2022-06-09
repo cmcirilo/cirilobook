70a22fcec194ac43259635518786b31c9cf4c664
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-06-09 14:45:14 -0300

    creating comments template

--- frontend/src/app/products/products.module.ts
+++ frontend/src/app/products/products.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CardModule } from '../components/card/card.module';
+import { MessageModule } from './../components/message/message.module';
+import { CommentsComponent } from './detail-product/comments/comments.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
 import { GridPhotoProductsComponent } from './grid-photo-products/grid-photo-products.component';
 import { ListProductsComponent } from './list-products/list-products.component';
 import { ProductComponent } from './product/product.component';
 import { ProductsRoutingModule } from './products-routing.module';
-import { DetailProductComponent } from './detail-product/detail-product.component';
-import { CommentsComponent } from './detail-product/comments/comments.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +18,12 @@ import { CommentsComponent } from './detail-product/comments/comments.component'
     DetailProductComponent,
     CommentsComponent,
   ],
-  imports: [CommonModule, ProductsRoutingModule, CardModule],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule,
+    CardModule,
+    MessageModule,
+    ReactiveFormsModule,
+  ],
 })
 export class ProductsModule {}
