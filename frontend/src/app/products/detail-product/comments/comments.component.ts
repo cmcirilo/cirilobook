3386623a032c252d1c58de7677c74b95cd02e4d2
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-06-10 08:56:10 -0300

    implementing comments save method in component

--- frontend/src/app/products/detail-product/comments/comments.component.ts
+++ frontend/src/app/products/detail-product/comments/comments.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 import { Comments } from './comment';
+import { CommentsService } from './comments.service';
 
 @Component({
   selector: 'app-comments',
@@ -8,9 +11,30 @@ import { Comments } from './comment';
   styleUrls: ['./comments.component.css'],
 })
 export class CommentsComponent implements OnInit {
+  @Input() id!: number;
   comments$!: Observable<Comments>;
+  commentForm!: FormGroup;
 
-  constructor() {}
+  constructor(
+    private commentsService: CommentsService,
+    private formBuilder: FormBuilder
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.comments$ = this.commentsService.get(this.id);
+    this.commentForm = this.formBuilder.group({
+      comment: [', Validators.maxLength(300'],
+    });
+  }
+
+  save(): void {
+    const comment = this.commentForm.get('comment')?.value ?? '';
+    this.comments$ = this.commentsService.add(this.id, comment).pipe(
+      switchMap(() => this.commentsService.get(this.id)),
+      tap(() => {
+        this.commentForm.reset();
+        alert('Comment saved');
+      })
+    );
+  }
 }
