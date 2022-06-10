f089bb1adbab21ddd98a9d23bc210c2f03af70dd
Carlos Marcelo Garcia Cirilo Junior <cmcirilo@gmail.com>
2022-06-10 17:49:06 -0300

    creating menu template

--- frontend/src/app/components/header/header.module.ts
+++ frontend/src/app/components/header/header.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MenuModule } from './../menu/menu.module';
 import { HeaderComponent } from './header.component';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MenuModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
